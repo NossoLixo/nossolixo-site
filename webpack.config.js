2718d41f7c92f0f6959fb552d54b4fb8a6a71dd3
Thiago Guimaraes <thiagogsr@gmail.com>
2016-12-05 22:50:02 -0300

    Setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+
+module.exports = {
+  entry: {
+    app: ['./app/main.js']
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/assets/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    inline: true,
+    contentBase: './build',
+    port: 3333
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['es2015', 'react']
+      }
+    }]
+  }
+}
