aa2dab9652086d6fcc1a1e7dcd4e04d0bb3a3d24
Thiago Guimaraes <thiagogsr@gmail.com>
2016-12-06 00:07:00 -0300

    Initial component

---
+++ app/components/Map.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+var Map = React.createClass({
+  render: function() {
+    return (
+      <div className="map">
+        <h1>Map</h1>
+      </div>
+    )
+  }
+});
+
+export default Map;
