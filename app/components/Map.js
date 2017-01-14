c43b7ff0d72c998120a6263ee597322032b15bb8
Thiago Guimaraes <thiagogsr@gmail.com>
2017-01-14 12:01:04 -0300

    Initial map

--- app/components/Map.js
+++ app/components/Map.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import {withGoogleMap, GoogleMap} from 'react-google-maps';
+
+const NossoLixoMap = withGoogleMap(props => (
+  <GoogleMap
+    defaultZoom={8}
+    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+  />
+));
 
 var Map = React.createClass({
   render: function() {
     return (
-      <div className="map">
-        <h1>Map</h1>
-      </div>
-    )
+      <NossoLixoMap
+        containerElement={
+          <div style={{ width: `100%`, height: `100%` }} />
+        }
+        mapElement={
+          <div style={{ height: `100%` }} />
+        }
+      />
+    );
   }
 });
 
