500d26367783783d56c5c36d9dec474ca6a63ba2
Thiago Guimaraes <thiagogsr@gmail.com>
2017-01-14 13:46:36 -0300

    Load places from api

--- app/components/Map.js
+++ app/components/Map.js
@@ -1,15 +1,65 @@
 import React from 'react';
-import {withGoogleMap, GoogleMap} from 'react-google-maps';
+import {withGoogleMap, GoogleMap, Marker} from 'react-google-maps';
+import co from 'co';
+import PlaceService from '../services/PlaceService';
 
 const NossoLixoMap = withGoogleMap(props => (
   <GoogleMap
-    defaultZoom={8}
-    defaultCenter={{ lat: -34.397, lng: 150.644 }}
-  />
+    defaultZoom={12}
+    defaultCenter={{
+      lat: -10.9919196,
+      lng: -37.0984939
+    }}
+  >
+    {props.markers.map((marker, index) => (
+      <Marker
+        title={marker.title}
+        position={marker.position}
+        key={index}
+      />
+    ))}
+  </GoogleMap>
 ));
 
-var Map = React.createClass({
-  render: function() {
+export default class Map extends React.Component {
+  constructor () {
+    super();
+    this.state = { markers: [] };
+
+    var fn = co.wrap(function* (val) {
+      return yield Promise.resolve(val);
+    });
+
+    var placeService = new PlaceService(),
+        buildMarkers = this.buildMarkers.bind(this);
+
+    fn(placeService.all())
+      .then(buildMarkers)
+      .catch(this.rescueError);
+  }
+
+  buildMarkers(response) {
+    var markers = [];
+
+    response.body.forEach(function(place) {
+      var marker = {
+        title: place.name,
+        position: {
+          lat: Number.parseFloat(place.lat),
+          lng: Number.parseFloat(place.lng)
+        }
+      };
+      markers.push(marker);
+    });
+
+    this.setState({ markers: markers });
+  }
+
+  rescueError(err) {
+    console.error(err.stack);
+  }
+
+  render() {
     return (
       <NossoLixoMap
         containerElement={
@@ -18,9 +68,9 @@ var Map = React.createClass({
         mapElement={
           <div style={{ height: `100%` }} />
         }
+        onMapLoad={this.handleMapLoad}
+        markers={this.state.markers}
       />
     );
   }
-});
-
-export default Map;
+}
