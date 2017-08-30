ba9d6f969fe6bec214d99ff183139a13df575c75
Thiago Guimarães <thiagogsr@gmail.com>
2017-08-30 10:34:32 -0300

    Create page for new place

--- app/server.js
+++ app/server.js
@@ -5,10 +5,12 @@ module.exports = {
   app: function () {
     const app = express();
     const indexPath = path.join(__dirname, '/../public/index.html');
+    const newPath = path.join(__dirname, '/../public/new.html');
     const publicPath = express.static(path.join(__dirname, '../public'));
 
     app.use('/public', publicPath);
     app.get('/', function (_, res) { res.sendFile(indexPath) });
+    app.get('/ponto-de-coleta', function (_, res) { res.sendFile(newPath) });
 
     return app;
   }
